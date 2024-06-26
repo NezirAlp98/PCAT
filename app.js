cd3c4bdb26f5b513a35497b543506415221b92d8
nezir alp arslan <nezirarslan1234@gmail.com>
2024-06-26 16:50:43 +0300

    middleware added

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-  console.log('function working');
-};
+const express=require('express');
+
+const app=express();
+
+//MIDDLEWARE
+app.use(express.static('public'))
+
+
+app.get('/',(req,res)=>{
+
+    res.sendFile(__dirname+'/temp/index.html')
+})
+
+
+
+const port=3000;
+app.listen(port,()=>console.log(`Sunucu ${port} portunda başlatıldı...`));
