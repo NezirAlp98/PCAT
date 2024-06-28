e315e89e23819b544cd5a848dae6db1800cd723e
nezir alp arslan <nezirarslan1234@gmail.com>
2024-06-28 17:24:39 +0300

    ejs added

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const express=require('express');
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
 
-const app=express();
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 //MIDDLEWARE
-app.use(express.static('public'))
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-app.get('/',(req,res)=>{
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-    res.sendFile(__dirname+'/temp/index.html')
-})
+app.get('/add', (req, res) => {
+  res.render('add');
+});
 
-
-
-const port=3000;
-app.listen(port,()=>console.log(`Sunucu ${port} portunda başlatıldı...`));
+const port = 3000;
+app.listen(port, () => console.log(`Sunucu ${port} portunda başlatıldı...`));
