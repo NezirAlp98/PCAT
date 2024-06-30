efd22ab48c81fcc6239b190ada291231b3d1bf42
nezir alp arslan <nezirarslan1234@gmail.com>
2024-06-30 17:37:29 +0300

    mongodb added

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const photo=require('./models/Photo');
+const Photo = require('./models/Photo');
+require('dotenv').config();
+
+
 const app = express();
+const db=process.env.DB_URI;
+
+//connect DB
+mongoose
+  .connect(
+    db
+  )
+  .then(() => console.log('MongoDB baglantisi kuruldu.'));
+
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
-//MIDDLEWARE
+//MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos=await Photo.find({});
+  res.render('index', {
+    photos
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  //console.log(req.params.id);
+  //res.render('photo');
+  const photo=await Photo.findById(req.params.id);
+  res.render('photo',{
+    photo
+  })
 });
 
 app.get('/about', (req, res) => {
@@ -18,6 +47,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
